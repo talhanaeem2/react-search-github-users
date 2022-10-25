df1e786b2097df3dec9f162c36d75001d326a908
Talha Naaeem <talha.naeem97@gmail.com>
2022-10-25 22:05:21 +0500

    Update PrivateRoute.js

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,8 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 const PrivateRoute = ({ children }) => {
   const { isAuhenticated, user } = useAuth0()
   const isUser = isAuhenticated && user
-  console.log(isAuhenticated)
-  if (!user) {
+  if (!isUser) {
     return <Navigate to='/login' />
   }
   return children
